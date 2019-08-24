c57d8bd75a38532380cc77895ecda8e5a96b5d3f
Shahrokh <shahorkhnabavi@gmail.com>
2019-08-24 19:00:10 +0200

    add winston logger

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const path = require('path');
-const fs = require('fs');
-const { createLogger, format, transports } = require('winston');
+"use strict";
+
 const config = require('./lib/Configuration');
-
-
-const logDir = 'log';
-
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
-}
-
-const filename = path.join(logDir, 'output.log');
-
-const logger = createLogger({
-    level: config.logger.level,
-    format: format.combine(
-        format.colorize(),
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.printf(info => ` -- ${info.timestamp} ${info.level}: ${info.message}`)
-    ),
-    transports: [
-        new transports.Console({
-            format: format.combine(
-                format.colorize(),
-                format.printf(
-                    info => ` - ${info.timestamp} ${info.level}: ${info.message}`
-                )
-            )
-        }),
-        new transports.File({ filename })
-    ]
-});
-
-logger.debug(`[DATE] Dedug: ${config.environment}`);
+const logger = require('./lib/Logger')(config);
 
 logger.debug('What rolls down stairs');
 logger.info('alone or in pairs,');
